29646a5a9064d9ec0802f321af72937d857be912
David Carew <carew@us.ibm.com>
2018-12-06 14:44:29 -0600

    Updated for ICP 3.1.1

--- src/main/webapp/utils.js
+++ src/main/webapp/utils.js
@@ -5,7 +5,7 @@ $('#user_name').keydown(function(e) {
     //POST request to API to create a new visitor entry in the database
     $.ajax({
         method: "POST",
-        url: "./api/visitors",
+        url: "/liberty-starter-ui/api/visitors",
         contentType: "application/json",
         data: JSON.stringify({
           name: name
@@ -31,7 +31,7 @@ $('#user_name').keydown(function(e) {
 
 //Retrieve all the visitors from the database
 function getNames() {
-  $.get("./api/visitors")
+  $.get("/liberty-starter-ui/api/visitors")
     .done(function(data) {
       if (data.length > 0) {
         data.forEach(function(element, index) {
